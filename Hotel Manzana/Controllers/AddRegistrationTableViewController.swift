d68f197a566e20fa3a6136002093a37b7e2e5b7d
Гость <Guest@mbp094.local>
2019-02-09 11:07:02 +0300

    DAsdgzshr

--- Hotel Manzana/Controllers/AddRegistrationTableViewController.swift
+++ Hotel Manzana/Controllers/AddRegistrationTableViewController.swift
@@ -14,6 +14,31 @@ class AddRegistrationTableViewController: UITableViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     
+    @IBOutlet weak var checkInLabel: UILabel!
+    @IBOutlet weak var checkInPicker: UIDatePicker!
+    @IBOutlet weak var checkOutLabel: UILabel!
+    @IBOutlet weak var checkOutPicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        checkInPicker.date = Date()
+        updateUI()
+    }
+    func updateUI() {
+        checkInPicker.date = max(checkInPicker.date, Date())
+        checkOutPicker.date = max(checkOutPicker.date, checkInPicker.date.addingTimeInterval(24 * 60 * 60))
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        
+        checkInLabel.text = dateFormatter.string(from: checkInPicker.date)
+        checkOutLabel.text = dateFormatter.string(from: checkOutPicker.date)
+    }
+    
     @IBAction func doneBarButtonTapped(_ sender: UIBarButtonItem) {
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
@@ -39,4 +64,11 @@ class AddRegistrationTableViewController: UITableViewController {
         print(#function, registration)
     }
     
+    @IBAction func checkInChanged(_ sender: UIDatePicker) {
+        updateUI()
+    }
+    
+    @IBAction func checkOutChanged(_ sender: UIDatePicker) {
+        updateUI()
+    }
 }
